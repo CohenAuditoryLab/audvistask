604e62fbb7d8bcc5f837edae154d3ceda93d6853
bkarpowicz3 <brianna.karpowicz@gmail.com>
2017-07-26 15:13:27 -0400

    parser function working on PC

--- ParseStim_AudVid.m
+++ ParseStim_AudVid.m
@@ -6,6 +6,7 @@ RCXFile = fullfile('C:\','work','AudVis_Task','AudVis_Circuit.rcx');
 
 % setup connection with TDT
 RP = actxcontrol('RPco.x',[5 5 26 26]);
+set(gcf, 'Visible', 'off');
 
 % attempt to connect to the RX6
 if RP.ConnectRX6('GB', 1)
@@ -31,7 +32,8 @@ end
 %% Initialize Serial Port to Receive Data
 
 delete(instrfindall);
-s = serial('COM1', 'BaudRate', 9600, 'DataBits', 8);
+s = serial('COM1', 'BaudRate', 9600, 'DataBits', 8, 'Parity', 'None',...
+    'StopBits',1,'FlowControl','None','Terminator', 'LF');%, 'TimeOut', .5);
 fopen(s);
 
 % set up data structures to store stimulus info
@@ -40,25 +42,28 @@ waveforms = cell(nTrials,1);
 maskers = cell(nTrials,1);
 freq = cell(nTrials,1);
 isH = zeros(nTrials,1);
-counter = 0;
+counter = 1;
 
 %% Begin session
 
 sessionRun = 1;
+quitNow = 0;
 
+disp('===================== Start of Session ========================');
 while sessionRun == 1
     %% Wait for data from serial port
-    disp('===================== Start of Session ========================');
+    disp(['========================== Trial ' num2str(counter) ' ============================']);
     
     checkingForStim = true;
     
     while checkingForStim
-        counter = counter + 1;
-        data = fscanf(s);
+        data = fscanf(s, '%s');
+        disp(['data received: ' data]);
         
         if length(data) >= 6
             try
                 [l, h, c, v, spk] = parse(data);
+                disp('parsed!')
             catch
                 disp('Stimulus string formatted incorrectly.')
                 continue
@@ -67,49 +72,62 @@ while sessionRun == 1
                 checkingForStim = false;
             end
         end
+        
+        if strcmp(data, 'done')
+            sessionRun = 0;
+            quitNow = 3;
+            break;
+        end
+    end
+    
+    if quitNow == 3
+        break;
     end
     
     %% Construct and upload stimulus to RX6
     [target_auditory, target_stimulus, frequencies, isHigh,...
         masker_auditory, masker_stimulus] = CreateStimulus(l, h, c, v);
     
-    [new_target, new_masker] = AddSpeakerCue(target_stimulus, masker_stimulus);
+    disp('Stimuli created!')
     
+    [new_target, new_masker] = AddSpeakerCue(target_stimulus, masker_stimulus);
+     
     % save stimulus info
-    waveforms(counter) = target_auditory;
-    maskers(counter) = masker_auditory;
-    freq(counter) = frequencies;
+    waveforms{counter} = target_auditory;
+    maskers{counter} = masker_auditory;
+    freq{counter} = frequencies;
     isH(counter) = isHigh;
     
     %% Trigger stimulus start
     
-    abortFlag = findTrialStart;
-    % if received the 'no' signal
-    if abortFlag == 1
-        % break out of trial and begin while loop over again
-        disp('Aborting Trial');
-        continue;
-    end
-    
     % LOAD stimuli to speakers/LEDs
     % if the first speaker is the target
     if strcmp(spk, 'ONE')
-        RP.WriteTagVEX('datain1', 0, 'F32', new_target(1,:));
+        %RP.WriteTagVEX('datain1', 0, 'F32', new_target(1,:));
         RP.WriteTagVEX('lightin1', 0, 'F32', new_target(2,:));
-        RP.WriteTagVEX('datain2', 0, 'F32', new_masker(1,:));
+        %RP.WriteTagVEX('datain2', 0, 'F32', new_masker(1,:));
         RP.WriteTagVEX('lightin2', 0, 'F32', new_masker(2,:));
+        drawnow;
     end
     % if the second speaker is the target
     if strcmp(spk, 'TWO')
-        RP.WriteTagVEX('datain1', 0, 'F32', new_masker(1,:));
+        %RP.WriteTagVEX('datain1', 0, 'F32', new_masker(1,:));
         RP.WriteTagVEX('lightin1', 0, 'F32', new_masker(2,:));
-        RP.WriteTagVEX('datain2', 0, 'F32', new_target(1,:));
+        %RP.WriteTagVEX('datain2', 0, 'F32', new_target(1,:));
         RP.WriteTagVEX('lightin2', 0, 'F32', new_target(2,:));
+        drawnow;
     end
     
-    % Play the stimulus
-    RP.SoftTrg(1); %Ch1
-    RP.SoftTrg(3); %Ch2
+    disp('Stimuli loaded!');
+    
+    abortFlag = findTrialStart;
+    if abortFlag == 2
+        disp('Playing');
+        % Play the stimulus
+        RP.SoftTrg(1); %Ch1
+        RP.SoftTrg(3); %Ch2
+        drawnow;
+    end
     
     % If you receive the stop stimulus
     abortFlag = findTrialStart;
@@ -118,24 +136,26 @@ while sessionRun == 1
         disp('Aborting Trial');
         RP.SoftTrg(2); %Ch1
         RP.SoftTrg(4); %Ch2
-        RP.ClearCOF();
+        drawnow;
+        counter = counter + 1;
         continue;
     end
 end
 
 % Close serial port and RX6
 RP.Halt;
+RP.ClearCOF;
 fclose(s);
 
 %% Save stimulus information to file
 
-stim_table = table((1:counter)', waveforms, maskers, freq, isH, 'VariableNames',...
+stim_table = table((1:nTrials)', waveforms, maskers, freq, isH, 'VariableNames',...
     {'trial', 'waveform','masker','tone_frequencies','isHigh'});
 data_folder = fullfile('C:', 'work', 'AudVis_Task', 'StimData');
 c = clock;
 save_filename = ['Stimulus_Data_', num2str(c(1)), num2str(c(2)), ...
     num2str(c(3)), '_', num2str(c(4)), num2str(c(5))];
-save([data_folder save_filename '_table.mat'], 'stim_table');
+save([data_folder filesep save_filename '_table.mat'], 'stim_table');
 
 %% Helper function for parsing data
 
@@ -152,12 +172,10 @@ save([data_folder save_filename '_table.mat'], 'stim_table');
         paramFlag = 0;
         
         % Parse string
-        tmpstr1 = strsplit(d,'START.');
-        tmpstr2 = strsplit(tmpstr1{2},'.STOP');
-        alldat = strsplit(tmpstr2{1},'.');
-        low = str2double(alldat{1});
-        high = str2double(alldat{2});
-        coh = str2double([alldat{3} alldat{4}])/100;
+        alldat = strsplit(d,'.');
+        low = str2double(alldat{2});
+        high = str2double(alldat{3});
+        coh = str2double(alldat{4})/100;
         vis = alldat{5};
         spk = alldat{6};
         paramFlag = 1;
@@ -169,15 +187,15 @@ save([data_folder save_filename '_table.mat'], 'stim_table');
         while checkAbortFlag == 1
             abortFlag = [];
             
-            dat2 = fscanf(sPort);
-            if strfind(dat2, 'no')
+            dat2 = fscanf(s, '%s');
+            if strcmp(dat2, 'no')
                 abortFlag = 1;
                 checkAbortFlag = 0;
             end
-            if strfind(dat2, 'go')
+            if strcmp(dat2, 'go')
+                abortFlag = 2;
                 checkAbortFlag = 0;
             end
         end
     end
-
 end
