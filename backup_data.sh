bb55b9f522b52431e5ce42de7fa38608c55d2ff9
bkarpowicz3 <brianna.karpowicz@gmail.com>
2017-08-01 13:51:23 -0400

    Delete backup_data.sh

--- backup_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Backing up data ..."
-rsync -auv /Users/briannakarpowicz/Documents/Cohen\ Lab/Auditory-Visual\ Task/ jaejin@130.91.169.205:/Volumes/KittyPride/VisualTones/
-echo "Done!"
-exit
